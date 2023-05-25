928fc624ec41832504281a2dfac3dd577925f491
leonellcruz7 <leonellcruz111513@gmail.com>
2023-05-25 09:37:03 +0800

    change server address

--- api/api.js
+++ api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://ec2-3-26-43-101.ap-southeast-2.compute.amazonaws.com:4000",
+  baseURL: "http://ec2-13-55-102-34.ap-southeast-2.compute.amazonaws.com:4000/",
+
   // baseURL: "http://localhost:4000",
 
   headers: {
